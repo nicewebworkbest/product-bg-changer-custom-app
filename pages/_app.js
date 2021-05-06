440906c16e75f4ab4dc889a482bfd0c450781db6
Nice Webwork <nicewebworkbest@gmail.com>
2021-05-06 11:17:43 -0400

    Completed

--- pages/_app.js
+++ pages/_app.js
@@ -1,38 +1,39 @@
+import React from 'react';
 import App from 'next/app';
 import Head from 'next/head';
 import { AppProvider } from '@shopify/polaris';
-import { Provider } from '@shopify/app-bridge-react'
+import { Provider } from '@shopify/app-bridge-react';
 import '@shopify/polaris/dist/styles.css';
-import Cookies from 'js-cookie';
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
-
-const client = new ApolloClient({
-	fetchOptions: {
-		credentials: 'include'
-	}
-});
+import translations from '@shopify/polaris/locales/en.json';
+import ClientRouter from '../components/ClientRouter';
+import '../assets/style.css';
 
 class MyApp extends App {
-	render() {
-		const { Component, pageProps } = this.props;
-		const config = {apiKey: API_KEY, shopOrigin: Cookies.get("shopOrigin"), forceRedirect: true };
-		return (
-			<React.Fragment>
-				<Head>
-					<title>Sample App</title>
-					<meta charSet='utf-8' />
-				</Head>
-				<Provider config={config}>
-					<AppProvider>
-						<ApolloProvider client={client}>
-							<Component {...pageProps} />
-						</ApolloProvider>
-					</AppProvider>
-				</Provider>
-			</React.Fragment>
-		);
-	}
+  render() {
+    const { Component, pageProps, shopOrigin } = this.props;
+
+    const config = { apiKey: API_KEY, shopOrigin, forceRedirect: true };
+    return (
+      <React.Fragment>
+        <Head>
+          <title>Product Background Changer</title>
+          <meta charSet="utf-8" />
+        </Head>
+        <Provider config={config}>
+          <ClientRouter />
+          <AppProvider i18n={translations}>
+            <Component {...pageProps} />
+          </AppProvider>
+        </Provider>
+      </React.Fragment>
+    );
+  }
+}
+
+MyApp.getInitialProps = async ({ ctx }) => {
+  return {
+    shopOrigin: ctx.query.shop,
+  }
 }
 
 export default MyApp;
--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 require('isomorphic-fetch');
 const dotenv = require('dotenv');
 dotenv.config();
-const port = parseInt(process.env.PORT, 10) || 3000;
-const dev = process.env.NODE_ENV !== 'production';
-
+// const {default: Shopify, ApiVersion} = require('@shopify/shopify-api');
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
 const serve = require('koa-static');
@@ -11,28 +9,56 @@ const mount = require('koa-mount');
 const next = require('next');
 const { default: createShopifyAuth } = require('@shopify/koa-shopify-auth');
 const { verifyRequest } = require('@shopify/koa-shopify-auth');
+const { ApiVersion } = require('@shopify/koa-shopify-graphql-proxy');
 const session = require('koa-session');
 const Router = require('koa-router');
 const { receiveWebhook, registerWebhook } = require('@shopify/koa-shopify-webhooks');
-const initDB = require('./server/database');
-const fs = require('fs');
-const path = require('path');
-const removeImageBackground = require('./server/services/removeImageBackground');
-const removeProductImage = require('./server/services/removeProductImage');
-const uploadProductImage = require('./server/services/uploadProductImage');
-const getImageMetafields = require('./server/services/getImageMetafields');
+
+const initDB = require('./server/src/database');
+const addGlobalBackgroundSettings = require('./server/addGlobalBackgroundSettings');
+const getGlobalBackgroundSettings = require('./server/getGlobalBackgroundSettings');
+const removeGlobalBackgroundSettings = require('./server/removeGlobalBackgroundSettings');
+const updateGlobalBackgroundSettings = require('./server/updateGlobalBackgroundSettings');
+const addBackgroundSettings = require('./server/addBackgroundSettings');
+const getBackgroundSettings = require('./server/getBackgroundSettings');
+const removeBackgroundSettings = require('./server/removeBackgroundSettings');
+const updateBackgroundSettings = require('./server/updateBackgroundSettings');
+
+const removeImageBackground = require('./server/src/services/removeImageBackground');
+const removeProductImage = require('./server/src/services/removeProductImage');
+const uploadProductImage = require('./server/src/services/uploadProductImage');
+const getImageMetafields = require('./server/src/services/getImageMetafields');
+const getBackgroundColor = require('./server/src/services/getBackgroundColor');
+
+const fs = require("fs");
+const path = require("path");
+
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
+
+// Shopify.Context.initialize({
+//   API_KEY: process.env.SHOPIFY_API_KEY,
+//   API_SECRET_KEY: process.env.SHOPIFY_API_SECRET_KEY,
+//   SCOPES: process.env.SHOPIFY_API_SCOPES.split(","),
+//   HOST_NAME: process.env.HOST.replace(/https:\/\//, ""),
+//   API_VERSION: ApiVersion.October20,
+//   IS_EMBEDDED_APP: true,
+//   SESSION_STORAGE: new Shopify.Session.MemorySessionStorage(),
+// });
 
 const {
   SHOPIFY_API_SECRET_KEY,
   SHOPIFY_API_KEY,
-	IMAGE_DIR_PATH
+  HOST,
+  IMAGE_DIR_PATH
 } = process.env;
 
 app.prepare().then(() => {
-	initDB();
 
+  initDB();
+  
   const server = new Koa();
   const router = new Router();
   server.use(session({ sameSite: 'none', secure: true }, server));
@@ -44,6 +70,7 @@ app.prepare().then(() => {
     createShopifyAuth({
       apiKey: SHOPIFY_API_KEY,
       secret: SHOPIFY_API_SECRET_KEY,
+      accessMode: 'offline',
       scopes: ['read_products', 'write_products'],
       async afterAuth(ctx) {
         const { shop, accessToken } = ctx.session;
@@ -52,6 +79,32 @@ app.prepare().then(() => {
           secure: true,
           sameSite: 'none'
         });
+
+        // Initialize Database
+        const global_setting = await getGlobalBackgroundSettings(shop);
+        if(global_setting.length) {
+          updateGlobalBackgroundSettings({
+            id: global_setting[0]._id,
+            bgColor: '255,77,255,0',
+            onoff: true,
+            accessToken: accessToken
+          });
+        } else {
+          addGlobalBackgroundSettings(shop, {
+            bgColor: '255,77,255,0',
+            onoff: true,
+            accessToken: accessToken
+          });
+        }
+
+        const additional_settings = await getBackgroundSettings(shop);
+        if(additional_settings.length) {
+          for(var i = 0; i < additional_settings.length; i++) {
+            await removeBackgroundSettings({
+              setting_id: additional_settings[i]._id
+            });
+          }
+        }
 
         const productUpdateHookRegistration = await registerWebhook({
           address: `${HOST}/webhooks/products/create-update`,
@@ -80,52 +133,49 @@ app.prepare().then(() => {
         } else {
           console.log('Failed to register product create webhook', productCreateHookRegistration.result);
         }
-
-        ctx.redirect('/');
+        
+        // ctx.redirect('/');
+        ctx.redirect(`/?shop=${shop}`);
       }
     })
   );
 
-	const webhook = receiveWebhook({ secret: SHOPIFY_API_SECRET_KEY });
+  const webhook = receiveWebhook({ secret: SHOPIFY_API_SECRET_KEY });
 
-	router.post('/webhooks/products/create-update', webhook, async (ctx) => {
-    const data = req.body.toString();
-		const productData = JSON.parse(data);
+  router.post('/webhooks/products/create-update', webhook, async (ctx) => {
+		const productData = ctx.request.body;
 		const productId = productData.id;
+    const global_setting = await getGlobalBackgroundSettings(ctx.state.webhook.domain);
+    const accessToken = global_setting[0].accessToken;
+    const bgColor = await getBackgroundColor(ctx.state.webhook.domain, productData.vendor);
+    console.log('bgColor', bgColor);
 		console.log('productId', productId);
 
 		for ( let index = 0; index < productData.images.length; index++ ) {
 			let image = productData.images[index];
-			console.log('image', image);
-			let imageMetafields = await getImageMetafields(ctx, image.id);
+			let imageMetafields = await getImageMetafields(ctx, accessToken, image.id);
 			let backgroundRemoved = imageMetafields.some(metafield => {
 				return (metafield.key == 'removed_bg') && (metafield.value == 'yes');
 			});
 			console.log('backgroundRemoved', backgroundRemoved);
 
 			if (!backgroundRemoved) {
-				let fileName = await removeImageBackground(image.src);
-				uploadProductImage(ctx, productId, image, fileName)
+				let fileName = await removeImageBackground(image.src, bgColor);
+				uploadProductImage(ctx, accessToken, productId, image, fileName)
 					.then((uploadedProductImage) => {
-						removeProductImage(ctx, productId, image.id);
+						removeProductImage(ctx, accessToken, productId, image.id);
 						console.log('uploadedProductImage', uploadedProductImage);
 					})
 					.catch((err) => {
 						console.log(err);
 					});
-
-				// for (let index = 0; index < image.variant_ids.length; index++) {
-				// 	let productVariant = await changeProductVariantImage(uploadedProductImage.id, image.variant_ids[index]);
-				// 	console.log(productVariant);
-				// }
 			}
 		}
 
-		res.sendStatus(200);
+		ctx.res.statusCode = 200;
   });
 
-	/* Delete all the images from the server */
-	router.get('/delete-images', async (req, res) => {
+  router.get('/delete-images', async (req, res) => {
 		const directory = IMAGE_DIR_PATH;
 
 		fs.readdir(directory, (err, files) => {
@@ -139,16 +189,60 @@ app.prepare().then(() => {
 				});
 			}
 		});
-		res.send('All the images were removed!');
+    ctx.response.message = "All the images were removed!";
 	});
 
-	router.get('/(.*)', verifyRequest(), async (ctx) => {
+  router.get('/get-global-background-settings', verifyRequest(), async (ctx) => {
+    const { shop } = ctx.session;
+    ctx.body = await getGlobalBackgroundSettings(shop);
+    ctx.res.statusCode = 200;
+  });
+
+  router.post('/add-global-background-settings', verifyRequest(), async (ctx) => {
+    const { shop } = ctx.session;
+    ctx.body = await addGlobalBackgroundSettings(shop, ctx.request.body);
+    ctx.res.statusCode = 200;
+  });
+
+  router.post('/update-global-background-settings', verifyRequest(), async (ctx) => {
+    ctx.body = await updateGlobalBackgroundSettings(ctx.request.body);
+    ctx.res.statusCode = 200;
+  });
+
+  router.delete('/delete-global-background-settings', verifyRequest(), async (ctx) => {
+    ctx.body = await removeGlobalBackgroundSettings(ctx.request.body);
+    ctx.res.statusCode = 200;
+  });
+
+  router.get('/get-background-settings', verifyRequest(), async (ctx) => {
+    const { shop } = ctx.session;
+    ctx.body = await getBackgroundSettings(shop);
+    ctx.res.statusCode = 200;
+  });
+
+  router.post('/add-background-settings', verifyRequest(), async (ctx) => {
+    const { shop } = ctx.session;
+    ctx.body = await addBackgroundSettings(shop, ctx.request.body);
+    ctx.res.statusCode = 200;
+  });
+
+  router.post('/update-background-settings', verifyRequest(), async (ctx) => {
+    ctx.body = await updateBackgroundSettings(ctx.request.body);
+    ctx.res.statusCode = 200;
+  });
+
+  router.delete('/delete-background-settings', verifyRequest(), async (ctx) => {
+    ctx.body = await removeBackgroundSettings(ctx.request.body);
+    ctx.res.statusCode = 200;
+  });
+
+  router.get('/(.*)', verifyRequest(), async (ctx) => {
     await handle(ctx.req, ctx.res);
     ctx.respond = false;
     ctx.res.statusCode = 200;
   });
 
-	server.use( mount( '/' + IMAGE_DIR_PATH, serve('./' + IMAGE_DIR_PATH) ) ) ;
+  server.use( mount( '/' + IMAGE_DIR_PATH, serve('./' + IMAGE_DIR_PATH) ) ) ;
 
   server.use(router.allowedMethods());
   server.use(router.routes());
@@ -156,5 +250,4 @@ app.prepare().then(() => {
   server.listen(port, () => {
     console.log(`> Ready on http://localhost:${port}`);
   });
-
 });
